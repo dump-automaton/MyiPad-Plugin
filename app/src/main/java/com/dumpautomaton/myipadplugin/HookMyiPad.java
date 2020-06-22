779ba79e351ade84c4ba7317f4d39ac2a38e8756
Richard-Zheng <yuqiu36@live.com>
2020-06-22 17:25:32 +0800

    hook to get classloader

--- app/src/main/java/com/dumpautomaton/myipadplugin/HookMyiPad.java
+++ app/src/main/java/com/dumpautomaton/myipadplugin/HookMyiPad.java
@@ -9,6 +9,7 @@ import java.lang.reflect.Method;
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 
 public class HookMyiPad implements IXposedHookLoadPackage {
@@ -17,36 +18,27 @@ public class HookMyiPad implements IXposedHookLoadPackage {
         // app load时调用
         // 匹配钩住的app的包名
         if (lpparam.packageName.equals("com.netspace.myipad")) {
-
-            // 加载app的指定类
-            final Class clazz = lpparam.classLoader.loadClass("com.netspace.library.utilities.HardwareInfo");
-
-            // getDeclaredMethods()应该是返回包含此类中所有方法的数组，遍历此数组
-            for (Method m : clazz.getDeclaredMethods()) {
-                Log.e("[HookMyiPad]","Method: " + m.getReturnType().getName() + " " + m.getName());
-
-                // 匹配遍历到的方法名称
-                if (m.getName().contains("getHardwareInfo")) {
-                    Log.e("[HookMyiPad]","GOT METHOD: " + clazz.getName() + " - " + m.getName());
-
-                    XposedBridge.hookMethod(m, new XC_MethodHook() {
-                        @Override
-                        protected void afterHookedMethod(MethodHookParam param) {
-                            // 在方法结束时拦截，并执行此处代码
-
-                            XposedBridge.log("[HookMyiPad]HOOKED METHOD: " + param.method.toString());
-                            Log.e("Xposed","HOOKED :" + param.thisObject);
-                            //Context context = (Context) AndroidAppHelper.currentApplication();
-                            Toast.makeText((Context) param.args[0], "Hooked", Toast.LENGTH_LONG).show();
-                            String str = HardwareInfo.getHardwareInfo((Context) param.args[0]);
-                            XposedBridge.log(str);
-
-                            // 设定返回值
-                            param.setResult(str);
-                        }
-                    });
+            XposedHelpers.findAndHookMethod("s.h.e.l.l.S", lpparam.classLoader, "attachBaseContext", Context.class, new XC_MethodHook() {
+                @Override
+                protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                    super.afterHookedMethod(param);
+                    //获取到Context对象，通过这个对象来获取classloader
+                    Context context = (Context) param.args[0];
+                    //获取classloader，之后hook加固后的就使用这个classloader
+                    ClassLoader realClassLoader = context.getClassLoader();
+                    //下面就是将classloader修改成壳的classloader就可以成功的hook了
+                    hookCheckoutXposed(realClassLoader);
                 }
-            }
+            });
         }
+    }
+
+    private void hookCheckoutXposed(ClassLoader classLoader) {
+        XposedHelpers.findAndHookMethod("com.netspace.library.utilities", classLoader, "isSkipELMCheck", new XC_MethodHook() {
+            @Override
+            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                param.setResult(false);
+            }
+        });
     }
 }
