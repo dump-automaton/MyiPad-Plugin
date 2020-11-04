4cb1eb952b81f661e262a6086403d0de36380bbb
Richard-Zheng <yuqiu36@live.com>
2020-11-04 20:05:27 +0800

    add showSyncEditDialog

--- app/src/main/java/com/dumpautomaton/myipadplugin/UtilsForHook.java
+++ app/src/main/java/com/dumpautomaton/myipadplugin/UtilsForHook.java
@@ -6,8 +6,35 @@ import android.content.DialogInterface;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
+import android.widget.EditText;
 
 public class UtilsForHook {
+
+    private static String mStringResult;
+    public static String showSyncEditDialog(String title, String defaultString, Context context) {
+        // make a handler that throws a runtime exception when a message is received
+        final Handler handler = new SyncDialogMessageHandler();
+        // make a text input dialog and show it
+        final EditText editText = new EditText(context);
+        AlertDialog.Builder alert = new AlertDialog.Builder(context);
+        alert.setTitle(title).setView(editText);
+        alert.show();
+        if (defaultString != null) {
+            editText.setText(defaultString);
+        }
+        alert.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                mStringResult = editText.getText().toString();
+                handler.sendMessage(handler.obtainMessage());
+            }
+        });
+        // loop till a runtime exception is triggered.
+        try {
+            Looper.loop();
+        } catch (RuntimeException e2) {
+        }
+        return mStringResult;
+    }
 
     private static boolean mResult;
     public static boolean showSyncBinaryDialog(String title, String message, Context context) {
