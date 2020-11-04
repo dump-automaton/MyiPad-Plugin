4ff39542ef5936bec5d7a7b4604cab4b4967c5ce
Richard-Zheng <yuqiu36@live.com>
2020-11-04 13:28:16 +0800

    move getHardwareInfoWithoutHardware

--- app/src/main/java/com/dumpautomaton/myipadplugin/MainActivity.java
+++ app/src/main/java/com/dumpautomaton/myipadplugin/MainActivity.java
@@ -12,6 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         TextView textView = (TextView) findViewById(R.id.textView);
-        textView.setText(HookMyiPad.getHardwareInfoWithoutHardware());
+        textView.setText(UtilsForHook.getHardwareInfoWithoutHardware());
     }
 }
