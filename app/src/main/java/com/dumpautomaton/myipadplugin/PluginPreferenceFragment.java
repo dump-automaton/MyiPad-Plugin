9394a60572390b2c65705fe720e3bf081d76c186
Richard-Zheng <yuqiu36@live.com>
2020-12-07 13:20:00 +0800

    add close button for preference

--- app/src/main/java/com/dumpautomaton/myipadplugin/PluginPreferenceFragment.java
+++ app/src/main/java/com/dumpautomaton/myipadplugin/PluginPreferenceFragment.java
@@ -1,8 +1,10 @@
 package com.dumpautomaton.myipadplugin;
 
+import android.app.Fragment;
 import android.content.ComponentName;
 import android.content.Intent;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
 
 public class PluginPreferenceFragment extends PreferenceFragment {
@@ -13,5 +15,15 @@ public class PluginPreferenceFragment extends PreferenceFragment {
         ComponentName component = new ComponentName("com.dumpautomaton.myipadplugin", "com.dumpautomaton.myipadplugin.MainActivity");
         xmlIntent.setComponent(component);
         addPreferencesFromIntent(xmlIntent);
+
+        Preference closePreference = findPreference("remove_fragment");
+        final Fragment thisFragment = this;
+        closePreference.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                getFragmentManager().beginTransaction().remove(thisFragment).commit();
+                return true;
+            }
+        });
     }
 }
