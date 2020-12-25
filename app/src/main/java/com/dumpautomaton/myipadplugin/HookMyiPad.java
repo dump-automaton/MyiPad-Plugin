4ac6d87d89718d5d793565696756c4781df9e49e
Richard-Zheng <yuqiu36@live.com>
2020-12-25 13:29:57 +0800

    tidy up

--- app/src/main/java/com/dumpautomaton/myipadplugin/HookMyiPad.java
+++ app/src/main/java/com/dumpautomaton/myipadplugin/HookMyiPad.java
@@ -1,18 +1,8 @@
 package com.dumpautomaton.myipadplugin;
 
-import android.content.ComponentName;
-import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
-
 import android.app.Application;
-import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.content.Context;
-import android.content.Intent;
-import android.os.Message;
-import android.widget.Toast;
+import android.os.Looper;
 
 import java.lang.reflect.Method;
 
@@ -27,72 +17,36 @@ public class HookMyiPad implements IXposedHookLoadPackage {
 
     @Override
     public void handleLoadPackage(LoadPackageParam lpparam) throws Exception {
-        // app load时调用
-        // 匹配钩住的app的包名
         if (lpparam.packageName.equals("com.netspace.myipad")) {
-            XposedBridge.log("[HookMyiPad]getting classLoader...");
-            XposedHelpers.findAndHookMethod("s.h.e.l.l.S", lpparam.classLoader, "onCreate", new XC_MethodHook() {
+            XposedHelpers.findAndHookMethod("android.app.Instrumentation", lpparam.classLoader, "newApplication", ClassLoader.class, String.class, Context.class, new XC_MethodHook() {
                 @Override
-                protected void afterHookedMethod(final MethodHookParam param) throws Throwable {
-                    Application appClz = (Application) param.thisObject;
-                    ClassLoader realClassLoader = appClz.getClassLoader();
-                    hookNewActivity(realClassLoader);
-                    hookHardwareInfo(realClassLoader);
+                protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                    Application app = (Application) param.getResult();
+                    if (!app.getClass().getName().contains("com.netspace")) {
+                        return;
+                    }
+                    ClassLoader realClassLoader = app.getClassLoader();
+                    hookHardwareInfo(realClassLoader, app);
                     hookAlertDialog(realClassLoader);
-                    hookAboutFragment(realClassLoader);
                 }
             });
         }
     }
 
-    private void hookNewActivity(ClassLoader realClassLoader) throws ClassNotFoundException {
-        Class<?> instrumentationClass = XposedHelpers.findClass("android.app.Instrumentation", realClassLoader);
-        Method method = XposedHelpers.findMethodExact(instrumentationClass, "newActivity",
-                ClassLoader.class, String.class, Intent.class);
-        XposedBridge.hookMethod(method, new ActivityHook());
-    }
-
-    private void hookHardwareInfo(final ClassLoader realClassLoader) throws ClassNotFoundException {
+    private void hookHardwareInfo(final ClassLoader realClassLoader, final Context context) throws ClassNotFoundException {
         final Class<?> clazz = realClassLoader.loadClass("com.netspace.library.utilities.HardwareInfo");
         Method m = XposedHelpers.findMethodExact(clazz, "getHardwareInfo", Context.class);
         XposedBridge.hookMethod(m, new XC_MethodHook() {
             @Override
             protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                final Activity activity = ActivityHook.getCurrentActivity();
                 if (Looper.myLooper() == null) {
                     Looper.prepare();
                 }
                 String result = UtilsForHook.showSyncEditDialog(Looper.myLooper(), "Plugin",
-                        (String)param.getResult(), UtilsForHook.getHardwareInfoWithoutHardware(), activity);
+                        (String) param.getResult(), UtilsForHook.getHardwareInfoWithoutHardware(), context);
                 if (result != null && !result.equals("")) {
-                    activity.runOnUiThread(new Runnable() {
-                        public void run() {
-                            Toast.makeText(activity, "Hooking!", Toast.LENGTH_LONG).show();
-                        }
-                    });
-
-                    try {
-                        param.setResult(result);
-                    } catch (Exception e) {
-                        activity.runOnUiThread(new Runnable() {
-                            public void run() {
-                                Toast.makeText(activity, "Hook failed!", Toast.LENGTH_LONG).show();
-                            }
-                        });
-                    }
+                    param.setResult(result);
                 }
-            }
-        });
-    }
-
-    private void hookAboutFragment(ClassLoader realClassLoader) {
-        XposedHelpers.findAndHookMethod("com.netspace.myipad.SettingsActivity$AboutFragment", realClassLoader, "onCreate", Bundle.class, new XC_MethodHook() {
-            @Override
-            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                Intent xmlIntent = new Intent();
-                ComponentName component = new ComponentName("com.dumpautomaton.myipadplugin", "com.dumpautomaton.myipadplugin.MainActivity");
-                xmlIntent.setComponent(component);
-                XposedHelpers.callMethod(param.thisObject, "addPreferencesFromIntent", xmlIntent);
             }
         });
     }
